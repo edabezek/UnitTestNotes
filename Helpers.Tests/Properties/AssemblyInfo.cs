58b6e57ca99194b4b3d01ae6d48530714df958e4
eda.bezek <eda_bezek@hotmail.com>
2022-09-26 15:18:17 +0300

    Add project files.

---
+++ Helpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eda59fba-a44d-463a-a4fc-b9b412adb0b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Helpers.Tests/StringHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        //girilen ifadenin başındaki ve sonundaki fazla boşluklar silinmelidir
+        [TestMethod]
+        public void girilen_ifadenin_basindaki_ve_sonundaki_fazla_bosluklar_silinmelidir()
+        {
+            //Arrange
+            var ifade = "   Salih Demiroğ  ";
+            var beklenen = "Salih Demiroğ";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            //Assert
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+        //girilen ifadenin içindeki fazla bosluklar silinmelidir
+        [TestMethod]
+        public void girilen_ifadenin_icindeki_fazla_bosluklar_silinmelidir()
+        {
+            //Arrange
+            var ifade = "Salih Demiroğ  Engin   Demiroğ      Ahmet  Sait              Duran";
+            var beklenen = "Salih Demiroğ Engin Demiroğ Ahmet Sait Duran";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            //Assert
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+    }
+}
