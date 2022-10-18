fe615b5df7ade4933d6320ee3bfa23613c0bb152
eda.bezek <eda_bezek@hotmail.com>
2022-10-18 11:10:46 +0300

    String Asserts

---
+++ AssertsString/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssertsString")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssertsString")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3dc17d92-9698-4317-a050-02c09d87be7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssertsString/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace AssertsString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StringContainsTest()
+        {
+            StringAssert.Contains("Test dünyasindan mrhb", "yes");
+        }
+        [TestMethod]
+        public void StringMatchesTest()
+        {
+            StringAssert.Matches("Test dünyasindan mrhb", new Regex(@"[a-zA-z]"));
+            StringAssert.DoesNotMatch("Test dünyasindan mrhb", new Regex(@"[0-9]"));
+        }
+        [TestMethod]
+        public void StartWithTest()
+        {
+            StringAssert.StartsWith("Test dünyasindan mrhb", "Test");
+        }
+        [TestMethod]
+        public void EndsWithTest()
+        {
+            StringAssert.EndsWith("Test dünyasindan mrhb", "mrb");
+        }
+    }
+}
